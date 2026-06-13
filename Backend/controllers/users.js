934afff6555da76cae3e8c2431f41ec49355ba39
Alireza Dehghanabnavi <alirezadehghan93@yahoo.com>
2026-06-13 10:33:48 +0200

    wrapped communications in try/catch

--- Backend/controllers/users.js
+++ Backend/controllers/users.js
@@ -2,8 +2,12 @@ const bcrypt = require('bcrypt')
 const usersRouter = require('express').Router()
 const User = require('../models/user')
 
-usersRouter.post('/', async (request, response) => {
-  const { username, email, name, password } =  request.body
+usersRouter.post('/', async (request, response, next) => {
+  const { username, email, name, password } = request.body
+
+  if (!password || password.length < 6) {
+    return response.status(400).json({ error: 'Password must be at least 6 characters long' })
+  }
 
   const saltRounds = 10
   const passwordHash = await bcrypt.hash(password, saltRounds)
@@ -15,14 +19,22 @@ usersRouter.post('/', async (request, response) => {
     passwordHash,
   })
 
-  const savedUser = await user.save()
+  try {
+    const savedUser = await user.save()
+    response.status(201).json(savedUser)
+  } catch (error) {
+    next(error)
+  }
 
-  response.status(201).json(savedUser)
 })
 
-usersRouter.get('/', async (request, response) => {
-  const users = await User.find({})
-  response.json(users)
+usersRouter.get('/', async (request, response, next) => {
+  try {
+    const users = await User.find({})
+    response.json(users)
+  } catch (error) {
+    next(error)
+  }
 })
 
 module.exports = usersRouter
